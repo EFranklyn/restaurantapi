aa4edc91e9245ee888bae4c593b9702cacf69e47
unknown <elisson.franklyn@gmail.com>
2021-10-30 12:46:53 -0300

    end testing recipes, group

--- recipe/tests.py
+++ recipe/tests.py
@@ -22,7 +22,8 @@ class RecipeTestCase(TestCase):
     """Tests for Recipes """
     def setUp(self):
         self.URLRECIPES = '/api/recipes/'
-        self.SEARCHRECIPES = '/api/searchrecipes/'
+        self.URLSEARCHRECIPES = '/api/searchrecipes/'
+        self.URLGROUPS = '/api/groups/'
         self.Ernane = Chef.objects.create(name='Ernane')
         self.pratos_de_domingo = GroupRecipe.objects.create(name='Pratos de domingo')
 
@@ -48,7 +49,7 @@ class RecipeTestCase(TestCase):
             group=self.sobremesas
         )
 
-        self.valid = {
+        self.valid_recipe = {
             "name":'Escondidinho de carne',
             "details":'Escondinho de carne com purê de mandioca',
             "ingredients":'3 Kg de carne seca, 800g de mandioca',
@@ -56,8 +57,8 @@ class RecipeTestCase(TestCase):
             "time":50,
             "chef":1,
             "chef_put":1,
-            "group":1,
-            "group_put":1
+            "group":2,
+            "group_put":2
         }
 
         self.invalid = {
@@ -71,11 +72,13 @@ class RecipeTestCase(TestCase):
 
         }
 
-        self.chef = {'name': 'Ernane'}
+        self.valid_group = {'name': 'Pratos de domingo'}
+
+        self.invalid_group = {'name': ''}
 
     def test_create_recipe(self):
         """Test create recipe"""
-        response = client.post('/api/recipes/', self.valid)
+        response = client.post('/api/recipes/', self.valid_recipe)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_recipes(self):
@@ -91,18 +94,17 @@ class RecipeTestCase(TestCase):
         serializer = RecipeSerializers(recipe)
         self.assertEqual(response.data, serializer.data)
 
-
     def test_valid_update_recipe(self):
         """Test update"""
         response = client.put(
             self.URLRECIPES + str(self.escondidinho.pk) + '/',
-            json.dumps(self.valid),
+            json.dumps(self.valid_recipe),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         recipe = Recipe.objects.get(pk=self.escondidinho.pk)
-        self.assertEqual(recipe.chef.pk, self.valid.get('chef'))
-        self.assertEqual(recipe.group.pk, self.valid.get('group'))
+        self.assertEqual(recipe.chef.pk, self.valid_recipe.get('chef'))
+        self.assertEqual(recipe.group.pk, self.valid_recipe.get('group'))
 
     def test_invalid_update_recipe(self):
         """test update false data"""
@@ -119,11 +121,56 @@ class RecipeTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_get_search_recipes(self):
-        """Testing the retrieve of all recipes"""
-        response = client.get(self.SEARCHRECIPES, params_search(), HTTP_ACCEPT='application/json')
+        """Test all searcherecipes"""
+        response = client.get(self.URLSEARCHRECIPES, params_search(), HTTP_ACCEPT='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        response = client.get(self.SEARCHRECIPES, {'name': self.escondidinho.name[5: 10]},
+        response = client.get(self.URLSEARCHRECIPES, params_search(name=self.escondidinho.name[5: 10]),
                               HTTP_ACCEPT='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
+        response = client.get(self.URLSEARCHRECIPES, params_search(chefname=self.escondidinho.name[0: 5]),
+                              HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = client.get(self.URLSEARCHRECIPES, params_search(groupname=self.escondidinho.name[0: 5]),
+                              HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_groups(self):
+        """test get Groups"""
+        response = client.get(self.URLGROUPS)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_specify_group(self):
+        response = client.get(self.URLGROUPS + str(self.pratos_de_domingo.pk) + '/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_group(self):
+        """Test create group"""
+        response = client.post(self.URLGROUPS, self.valid_group)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = client.post(self.URLGROUPS, self.invalid_group)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_group(self):
+        """Test update Group"""
+        response = client.put(
+            self.URLGROUPS + str(self.sobremesas.pk) + '/',
+            json.dumps(self.valid_group),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        group = GroupRecipe.objects.get(pk=self.sobremesas.pk)
+        self.assertEqual(group.name, self.valid_group.get('name'))
+
+    def test_delete_group(self):
+        """Test delete group"""
+        response = client.delete(self.URLRECIPES + str(self.canjica.pk) + '/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = client.delete(self.URLGROUPS + str(self.sobremesas.pk) + '/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+
